8f0367989352871500550aa58d7cee6b8b872bff
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-10-30 20:54:11 +0100

    save todos in json file

---
+++ TodoService.cs
@@ -0,0 +1,48 @@
+
+
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json.Serialization;
+using TodoList.Pages;
+
+public class TodoService
+{
+    private List<TodoItem> todos = new List<TodoItem>();
+
+    public List<TodoItem> GetTodos()
+    {
+        return todos;
+    }
+
+
+    public void AddTodo(TodoItem todo)
+    {
+        todos.Add(todo);
+        SaveTodos();
+    }
+
+    public void DeleteTodo(TodoItem todo)
+    {
+        todos.Remove(todo);
+        SaveTodos();
+    }
+
+
+
+    public List<TodoItem> GetTodoFromFile()
+    {
+        string json = System.IO.File.ReadAllText("todos.json");
+        if (string.IsNullOrEmpty(json))
+        {
+            return new List<TodoItem>();
+        }
+
+        return System.Text.Json.JsonSerializer.Deserialize<List<TodoItem>>(json);
+    }
+
+    private void SaveTodos()
+    {
+        string json = System.Text.Json.JsonSerializer.Serialize(todos);
+        System.IO.File.WriteAllText("todos.json", json);
+    }
+
+}
